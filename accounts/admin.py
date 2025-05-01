35ecd220b774428a333f67d686a0db349451a95e
Faiza Ahmed Esha <140197867+Faiza-ahmed-esha@users.noreply.github.com>
2025-05-01 22:11:01 +0600

    updated settings and Registered custom usermodel in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_type', 'is_staff', 'is_active')
+    list_filter = ('user_type', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'age')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('User Type', {'fields': ('user_type', 'license_number')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'user_type'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
