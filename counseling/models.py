4d1811b27132ba1dd34c1a2139beab4bda08f637
Anika-22201115 <anikaera2003@gmail.com>
2025-05-01 16:26:21 +0600

    com

--- counseling/models.py
+++ counseling/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-class Counselor(models.Model):
-    name = models.CharField(max_length=100)
-    specialization = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    available = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
