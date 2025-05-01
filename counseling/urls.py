1e242b8287c28cd2226f4de98e603a1e4ea8fa3b
Faiza Ahmed Esha <140197867+Faiza-ahmed-esha@users.noreply.github.com>
2025-05-02 00:19:36 +0600

    initial error solve and authentication complete

---
+++ counseling/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.http import HttpResponse
+     
+def empty_view(request):
+    return HttpResponse("This section is under construction")
+
+urlpatterns = [
+    path('', empty_view, name='counseling_home'),
+             ]
