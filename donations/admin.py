5044624dd5d03b7ec79a57d746d93e4c94644ffa
Tamanna <22201137@uap-bd.edu>
2025-05-02 22:23:15 +0600

    url pattern

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'donor_display', 'amount', 'currency', 'payment_method', 'status', 'created_at')
+    list_filter = ('status', 'payment_method', 'currency', 'is_anonymous', 'created_at')
+    search_fields = ('donor_name', 'donor_email', 'donor_phone', 'transaction_id')
+    date_hierarchy = 'created_at'
+
+    def donor_display(self, obj):
+        if obj.is_anonymous:
+            return "Anonymous"
+        return obj.donor_name or f"User #{obj.user_id}" if obj.user else "Anonymous"
+
+    donor_display.short_description = 'Donor'
